4116f821308c9db819ef027c0dd565e8ca42fa8d
Andrew Rice <andrewrice@Andrews-MacBook-Pro.local>
2017-04-04 18:45:13 -0400

    Outline Entry class

---
+++ JParse/Entry.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import re
+
+class Entry:
+    """"
+    Entry describes an entry from the dictionary.
+    Requires word (in Japanese) to be passed during creation.
+    Populates following variables with information:
+        word = original word (REQUIRED)
+        reading = full reading including ending (ex: 食べる and たべる) 
+        furigana = furigana for the kanji only (or None when word is full kana)
+        kanji_stem = only the kanji, no extra kana (ex: 食べる is just 食)
+    """
+    # Look up entry in dictionary and populate class variables with information
+    def __init__(self, word, reading=None):
+        self.word = word
+        self.furigana = _furi(reading)
+        self.kanji_stem = _kanji(word)
+
+    # Method to lookup a word in the dictionary
+    def lookup(self, word):
+        # Connect to DB
+        # Get entry
+        # Parse into variables
+        # Check if furigana exists, or look it up if needed. Either way, compare
+        if not self.furigana:
+            pass
+
+    # Outputs string as: Word (Furigana) {Properties} [Definitions]
+    def __str__(self):
+        # set properties variable as , separated string here
+        #properties =
+        return "{word} ({furigana}) {{properties}} [{definitions}]"
+
+    def _furi(self, reading):
+        if reading:
+            same = ''.join(set(self.word) & set(reading))
+            furi = re.sub(same, '', reading)
+            return furi
+        else:
+            return None
+
+    def _kanji(self, word):
+        pass
--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 1. Install MeCab
 2. Install CaboCha
 3. Build CaboCha python (python3 setup.py install)
-
